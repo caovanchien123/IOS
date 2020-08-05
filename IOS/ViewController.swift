231c42130ffa5f5d9c0072b71c19bfc660bc9152
caovanchien123 <49179638+caovanchien123@users.noreply.github.com>
2020-08-05 03:00:46 -0700

    Initial Commit

---
+++ IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS
+//
+//  Created by x_x on 8/5/20.
+//  Copyright © 2020 x_x. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
